eafed11ca53a8d3bf1efc100ff8f73edeaadabf2
archana-hutech <archana@hutechsolutions.com>
2025-01-08 15:18:23 +0530

    node with graphql API added

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const port = process?.env?.PORT || 5001
-
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const port = process?.env?.PORT || 5001;
 const app = express();
+const { makeExecutableSchema } = require("graphql-tools");
+const {graphiqlExpress, graphqlExpress} = require("apollo-server-express")
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ limit:"2mb",extended: true }));
+app.use(bodyParser.urlencoded({ limit: "2mb", extended: true }));
 
-app.get('/', (req, res) =>{
-    res.send('Welcome to the Express Server!')
-})
+//define the type
+const typeDefinition = `
+      type Query{
+      greeting:String
+      }`
 
-app.listen(port,() =>{
-    console.log("server listening on port", port);   
-})
+const resolverObject = {
+  Query: {
+    greeting: () => "Hello, World!"
+  }
+}
+
+const schema = makeExecutableSchema({
+  typeDefs: typeDefinition,
+  resolvers: resolverObject,
+});
+
+app.use('/graphql', graphqlExpress({schema}))
+app.use('/graphiql', graphiqlExpress({endpointURL:'/graphql'}))
+app.get("/", (req, res) => {
+  res.send("Welcome to the Express Server!");
+});
+
+app.listen(port, () => {
+  console.log("server listening on port", port);
+});
